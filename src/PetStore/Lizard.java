3ffba83e9d4d8a022ebc510516a315567850a8c6
trist <trho0001@stud.ek.dk>
2025-09-15 13:50:02 +0200

    Subclasses

--- src/PetStore/Lizard.java
+++ src/PetStore/Lizard.java
@@ -1,4 +1,18 @@
 package PetStore;
 
 public class Lizard {
+    private String sound;
+
+    public Lizard(String name, String sound);{
+        this.sound = "Hiss";
+
+
+    }
+
+    @Override
+    public String toString(){return "Cat: " + sound;
+
+    }
+
+
 }
